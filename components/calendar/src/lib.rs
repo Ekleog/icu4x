f1fb242b13be9f6da27cba10fbdfa23aeef8a1da
Léo Gaspard <leo@gaspard.io>
2023-02-19 01:36:04 +0100

    reduce

--- components/calendar/src/lib.rs
+++ components/calendar/src/lib.rs
@@ -1,1 +1,18 @@
-use icu_provider::yoke;
+fn try_from(other: Option<()>) -> Result<(), DataError> {
+    let o: Option<Result<(), DataError>> = match other {
+        Some(o) => loop {},
+        None => None,
+    };
+    o.transpose();
+    loop {}
+}
+enum DataErrorKind {
+    MissingDataKey,
+    MismatchedType(&'static [()]),
+    Io(u8),
+}
+pub struct DataError {
+    kind: DataErrorKind,
+    key: Option<()>,
+}
+pub use icu_provider::yoke;
--- provider/core/src/lib.rs
+++ provider/core/src/lib.rs
@@ -1,18 +1,2 @@
-fn try_from(other: Option<()>) -> Result<(), DataError> {
-    let o: Option<Result<(), DataError>> = match other {
-        Some(o) => loop {},
-        None => None,
-    };
-    o.transpose();
-    loop {}
-}
-enum DataErrorKind {
-    MissingDataKey,
-    MismatchedType(&'static [()]),
-    Io(u8),
-}
-pub struct DataError {
-    kind: DataErrorKind,
-    key: Option<()>,
-}
+
 pub use yoke;
