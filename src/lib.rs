db060f38e084edbb18b084cd6210e2404bf5e217
Léo Gaspard <leo@gaspard.io>
2023-02-19 02:05:21 +0100

    reduce

--- src/lib.rs
+++ src/lib.rs
@@ -11,12 +11,12 @@ enum Opt1 {
     Some(Res1),
 }
 enum Res1 {
-    Ok(()),
+    Ok,
     Err(Error),
 }
 fn foo(other: Opt1) -> Result<Option<()>, Error> {
     match other {
-        Opt1::Some(Res1::Ok(foo)) => Ok(Some(foo)),
+        Opt1::Some(Res1::Ok) => Ok(Some(())),
         Opt1::Some(Res1::Err(foo)) => Err(foo),
         Opt1::None => Ok(None),
     }
