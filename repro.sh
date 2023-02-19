61dce6684eeaa4ef7d3dd0cd524a26a2e785da2e
Léo Gaspard <leo@gaspard.io>
2023-02-19 02:30:00 +0100

    reduce

--- repro.sh
+++ repro.sh
@@ -4,4 +4,4 @@ exec /home/ekleog/.rustup/toolchains/stage1/bin/rustc \
     src/lib.rs \
     --emit=link \
     -C opt-level=2 \
-    --out-dir ./target --cfg fuzzing -Cdebug-assertions -Ctarget-cpu=native -Cdebuginfo=2 -Coverflow_checks -Clink-dead-code -Cpasses=sancov-module -Cllvm-args=-sanitizer-coverage-inline-8bit-counters -Cllvm-args=-sanitizer-coverage-level=3 -Cllvm-args=-sanitizer-coverage-pc-table -Cllvm-args=-sanitizer-coverage-trace-compares -Zsanitizer=address
+    --out-dir ./target -Cdebug-assertions -Ctarget-cpu=native -Cdebuginfo=2 -Coverflow_checks -Clink-dead-code -Cpasses=sancov-module -Cllvm-args=-sanitizer-coverage-inline-8bit-counters -Cllvm-args=-sanitizer-coverage-level=3 -Cllvm-args=-sanitizer-coverage-pc-table -Cllvm-args=-sanitizer-coverage-trace-compares -Zsanitizer=address
