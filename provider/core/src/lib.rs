bef6ff2a78b707c4daf79f0630c2e3cdd506d7ce
Léo Gaspard <leo@gaspard.io>
2023-02-19 01:26:50 +0100

    reduce

--- provider/core/src/lib.rs
+++ provider/core/src/lib.rs
@@ -1,45 +1,33 @@
-mod any {
-    use crate::prelude::*;
-    pub struct AnyPayload;
-    struct AnyResponse {
-        payload: Option<AnyPayload>,
-    }
-    impl TryFrom<DataResponse> for AnyResponse {
-        type Error = DataError;
-        fn try_from(other: DataResponse) -> Result<Self, Self::Error> {
-            Ok(Self {
-                payload: other.payload.map(|p| p.try_unwrap_owned()).transpose()?,
-            })
-        }
+
+pub struct AnyPayload;
+struct AnyResponse {
+    payload: Option<AnyPayload>,
+}
+impl TryFrom<DataResponse> for AnyResponse {
+    type Error = DataError;
+    fn try_from(other: DataResponse) -> Result<Self, Self::Error> {
+        Ok(Self {
+            payload: other.payload.map(|p| p.try_unwrap_owned()).transpose()?,
+        })
     }
 }
-mod error {
-    use crate::prelude::*;
-     enum DataErrorKind {
-        MissingDataKey,
-        MismatchedType(&'static [()]),
-        Io(u8),
-    }
-    pub struct DataError {
-        kind: DataErrorKind,
-        key: Option<()>,
+enum DataErrorKind {
+    MissingDataKey,
+    MismatchedType(&'static [()]),
+    Io(u8),
+}
+pub struct DataError {
+    kind: DataErrorKind,
+    key: Option<()>,
+}
+pub struct DataResponseMetadata;
+pub struct DataPayload;
+impl DataPayload {
+    pub fn try_unwrap_owned(self) -> Result<AnyPayload, DataError> {
+        loop {}
     }
 }
-mod response {
-    use crate::error::{DataError, };
-    pub struct DataResponseMetadata;
-    pub struct DataPayload;
-    impl DataPayload {
-        pub fn try_unwrap_owned(self) -> Result<crate::any::AnyPayload, DataError> {loop{}}
-    }
-    pub struct DataResponse {
-        pub payload: Option<DataPayload>,
-    }
+pub struct DataResponse {
+    pub payload: Option<DataPayload>,
 }
-mod prelude {
-    pub use crate::error::DataError;
-    pub use crate::response::DataResponse;
-    pub use crate::response::DataResponseMetadata;
-    pub use yoke;
-}
-pub use prelude::*;
+pub use yoke;
