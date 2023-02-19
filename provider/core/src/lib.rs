f71230d880cc341ab7e0775a4537f8fd0b312aab
Léo Gaspard <leo@gaspard.io>
2023-02-19 01:32:26 +0100

    reduce

--- provider/core/src/lib.rs
+++ provider/core/src/lib.rs
@@ -1,11 +1,10 @@
-
 pub struct AnyPayload;
 struct AnyResponse;
-impl AnyResponse {
-    fn try_from(other: Option<()>) -> Result<Self, DataError> {
-        other.map(|p| -> Result<AnyPayload, DataError> { loop {} }).transpose();
-        loop {}
-    }
+fn try_from(other: Option<()>) -> Result<AnyResponse, DataError> {
+    other
+        .map(|p| -> Result<AnyPayload, DataError> { loop {} })
+        .transpose();
+    loop {}
 }
 enum DataErrorKind {
     MissingDataKey,
