7a90f41c75f382bc2bf854124fbdfc46603350d9
Léo Gaspard <leo@gaspard.io>
2023-02-19 23:31:21 +0100

    attempt reproing rtt'ing through .ll

--- llc-repro/repro.sh
+++ llc-repro/repro.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
-~/prog/rust/build/x86_64-unknown-linux-gnu/llvm/bin/llc main.bc
+#~/prog/rust/build/x86_64-unknown-linux-gnu/llvm/bin/llc main.bc
+export PATH="$HOME/prog/rust/build/x86_64-unknown-linux-gnu/llvm/bin:$PATH"
+llvm-dis main.bc --preserve-ll-uselistorder -o disas.ll
+llvm-as disas.ll --preserve-bc-uselistorder -o disas.bc
+llc disas.bc
