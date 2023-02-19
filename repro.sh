533b6862869a8afeab37c13f5fa48afd6405522e
Léo Gaspard <leo@gaspard.io>
2023-02-19 02:34:14 +0100

    reduce

--- repro.sh
+++ repro.sh
@@ -5,4 +5,9 @@ exec /home/ekleog/.rustup/toolchains/stage1/bin/rustc \
     --emit=link \
     -C opt-level=2 \
     --out-dir ./target \
-    -C debuginfo=1 -Clink-dead-code -Cpasses=sancov-module -Cllvm-args=-sanitizer-coverage-inline-8bit-counters -Cllvm-args=-sanitizer-coverage-level=3 -Cllvm-args=-sanitizer-coverage-pc-table -Cllvm-args=-sanitizer-coverage-trace-compares -Zsanitizer=address
+    -C debuginfo=1 \
+    -Clink-dead-code \
+    -Cpasses=sancov-module \
+    -Cllvm-args=-sanitizer-coverage-inline-8bit-counters \
+    -Cllvm-args=-sanitizer-coverage-level=3 \
+    -Cllvm-args=-sanitizer-coverage-pc-table -Cllvm-args=-sanitizer-coverage-trace-compares -Zsanitizer=address
